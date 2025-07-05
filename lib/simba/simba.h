c0ebb9f2fa24e0c236dee4fae28ea2d72ed17886
Mateusz Krajewski <matikrajek42@gmail.com>
2025-07-05 08:06:35 +0200

    fix files in release

--- lib/simba/simba.h
+++ lib/simba/simba.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_SIMBA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_SIMBA_XML_HASH 5035221874803753313
+#define MAVLINK_SIMBA_XML_HASH -4476815674272015630
 
 #ifdef __cplusplus
 extern "C" {
